1759958bbffab8474726a49c32f6b13fa95ef248
waron97 <winkler.aron5gmail.com>
2023-07-20 14:29:30 +0200

    change default bert model

--- src/bert/app/util/constants.py
+++ src/bert/app/util/constants.py
@@ -1,7 +1,7 @@
 import os
 
 MAX_QUERY_SIZE = int(os.getenv('MAX_QUERY_SIZE', 100))
-BERT_MODEL = os.getenv('BERT_MODEL', 'bert-base-uncased')
+BERT_MODEL = os.getenv('BERT_MODEL', 'distilbert-base-multilingual-cased')
 LOGS_URL = "http://logs:8080"
 LOGS_APP_NAME = os.getenv("LOGS_APP_NAME")
 LOGS_API_KEY = os.getenv("LOGS_KEY")
