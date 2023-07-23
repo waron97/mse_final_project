862396d90ea6e7a39d8615d3442ba628d7794f19
waron97 <winkler.aron5gmail.com>
2023-07-23 11:45:31 +0200

    remove old function

--- src/indexer/src/tasks/encoding/StoredDocument.go
+++ src/indexer/src/tasks/encoding/StoredDocument.go
@@ -2,8 +2,6 @@ package encoding
 
 import (
 	"indexer/src/util/core"
-	"indexer/src/util/storage"
-	"os"
 )
 
 type Passage struct {
@@ -14,49 +12,4 @@ type Passage struct {
 type StoredDocument struct {
 	DocId    string    `json:"docId"`
 	Passages []Passage `json:"passages"`
-}
-
-func NewDocumentFromId(docId string) *StoredDocument {
-	passages := getDocumentPassages(docId)
-
-	return &StoredDocument{
-		DocId:    docId,
-		Passages: passages,
-	}
-}
-
-func NewDocumentsFromIds(docIds []string) []*StoredDocument {
-	docsChan := make(chan *StoredDocument)
-	docs := make([]*StoredDocument, len(docIds))
-
-	for i, docId := range docIds {
-		go func(i int, docId string) {
-			docsChan <- NewDocumentFromId(docId)
-		}(i, docId)
-	}
-
-	for i := 0; i < len(docIds); i++ {
-		docs[i] = <-docsChan
-	}
-
-	return docs
-}
-
-func getDocumentPassages(docId string) []Passage {
-	constants := core.GetConstants()
-	docPath := constants.StorageDocsDir + "/" + docId
-	files, err := os.ReadDir(docPath)
-	core.ErrPanic(err)
-	passages := make([]Passage, len(files))
-	for i, file := range files {
-		var passageEmbeddings []core.Vector
-		var passageId string = file.Name()
-		storagePath := docPath + "/" + passageId
-		storage.ReadStructFromFile(storagePath, &passageEmbeddings)
-		passages[i] = Passage{
-			PassageId:  passageId,
-			Embeddings: passageEmbeddings,
-		}
-	}
-	return passages
 }
